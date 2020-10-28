4eacc3eb1677940a9450c1458f60d606ab8f2523
Simon Bustamante <simon.bustamante@smartmatic.com>
2020-10-28 17:33:20 -0500

    10.3 the conference controller on index will call twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,18 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ConferenceRepository;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}", name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function index(string $name = ''): Response
+    //public function index(string $name = ''): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
     {
         // return $this->render('conference/index.html.twig', [
         //     'controller_name' => 'ConferenceController',
         // ]);
-        $greet = '';
+        /*$greet = '';
         if ($name) {
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         }
@@ -29,6 +32,9 @@ class ConferenceController extends AbstractController
                 </body>
             </html>
             EOF
-        );
+        );*/
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
 }
