38381aeee1bf2baa9bcc6a49a0596caf76f95870
Simon Bustamante <simon.bustamante@smartmatic.com>
2020-10-29 09:58:35 -0500

    10.7

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -23,14 +23,20 @@ class ConferenceController extends AbstractController
             'conferences' => $conferenceRepository->findAll(),
         ]));
     }
-     /**
-      * @Route("/conference/{id}", name="conference")
+    /**
+     * @Route("/conference/{id}", name="conference")
      */
-    public function show(Environment $twig, Conference $conference,CommentRepository $commentRepository)
+    public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository)
     {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
         return new Response($twig->render('conference/show.html.twig', [
             'conference' => $conference,
-            'comments' => $commentRepository->findBy(['conference' =>$conference], ['createdAt' => 'DESC']),
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset +
+                CommentRepository::PAGINATOR_PER_PAGE),
         ]));
     }
 }
