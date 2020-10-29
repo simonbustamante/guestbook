531a469bd3dfd2bff67d6dd69957c928527ff869
Simon Bustamante <simon.bustamante@smartmatic.com>
2020-10-29 09:43:49 -0500

    10.5

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -19,22 +19,6 @@ class ConferenceController extends AbstractController
     //public function index(string $name = ''): Response
     public function index(Environment $twig, ConferenceRepository $conferenceRepository)
     {
-        // return $this->render('conference/index.html.twig', [
-        //     'controller_name' => 'ConferenceController',
-        // ]);
-        /*$greet = '';
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-        return new Response(<<<EOF
-            <html>
-                <body>
-                    $greet
-                    <img src="/images/under-construction.gif" />
-                </body>
-            </html>
-            EOF
-        );*/
         return new Response($twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
