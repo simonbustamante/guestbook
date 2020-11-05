34edd2bde1a79cfe6e92e1454711a4264a6df720
Simon Bustamante <simon.bustamante@smartmatic.com>
2020-11-05 10:32:15 -0500

    17.6 Trabajando con una base de datos de prueba

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ConferenceControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /*public function testIndex()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
@@ -21,5 +21,20 @@ class ConferenceControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/conference/{slug}');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }*/
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+        //$client->click($crawler->filter('h4 + p a')->link());
+
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 }
